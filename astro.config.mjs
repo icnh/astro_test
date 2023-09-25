dbd24f8d73954ad1897902df6724cb7c05aa1797
Stefan Matthias Aust <sma@icnh.de>
2023-09-25 13:41:44 +0200

    activate github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://icnh.github.io',
+    base: '/astro_test',
+});
