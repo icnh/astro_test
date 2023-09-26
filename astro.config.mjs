1ae719e5ec8e0a8077c70d06c1fdcc273b7449db
Stefan Matthias Aust <eibaan@gmail.com>
2023-09-26 10:53:56 +0200

    trying again to remove the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://icnh.github.io',
-    base: '', // '/astro_test',
+    // base: '/astro_test',
 });
